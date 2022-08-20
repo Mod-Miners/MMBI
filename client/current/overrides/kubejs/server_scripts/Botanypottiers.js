86b98df46fc33a499914ce7798783b7b2ac89317
Syntheticdev <35655841+Synthetic-Dev@users.noreply.github.com>
2022-08-20 11:15:37 +0100

    remove/add

--- client/current/overrides/kubejs/server_scripts/Botanypottiers.js
+++ client/current/overrides/kubejs/server_scripts/Botanypottiers.js
@@ -1,14 +1,11 @@
-// priority: 0
+// settings.logAddedRecipes = true
+// settings.logRemovedRecipes = true
+// settings.logSkippedRecipes = false
+// settings.logErroringRecipes = true
 
-settings.logAddedRecipes = true
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
-settings.logErroringRecipes = true
-
-console.info('Hello, World! (You will see this line every time server resources reload)')
+// console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
-  // Change recipes here
   event.shaped('1x ftb-power-pots:power_pot_mk1', [
     ' C ',
     'BPB',
@@ -49,13 +46,4 @@ onEvent('recipes', event => {
       P: 'ftb-power-pots:power_pot_mk3',
       I: '#forge:ingots/refined_obsidian'
     })
-})
-
-
-onEvent('item.tags', event => {
-  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
-  // event.get('forge:cobblestone').add('minecraft:diamond_ore')
-
-  // Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-  // event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 })
